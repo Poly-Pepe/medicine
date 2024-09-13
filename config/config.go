b2a587ac8e46412f514b0b4f6cb17c2b79f6c17d
binary0bun <mchibyshev@dc2b.ru>
2024-09-14 01:13:42 +0300

    add ci

--- config/config.go
+++ config/config.go
@@ -4,11 +4,13 @@ import (
 	"github.com/caarlos0/env/v9"
 )
 
+//todo: это конфиги для теста
+
 //nolint:lll //envDefault.
 type Config struct {
 	AppPort     int    `env:"APP_PORT" envDefault:"8004"`
 	LogLevel    string `env:"LOG_LEVEL" envDefault:"info"`
-	DataBaseDNS string `env:"DATABASE_DSN" envDefault:"postgresql://architect:09970207@localhost:5444/medecine?sslmode=disable"`
+	DataBaseDNS string `env:"DATABASE_DSN" envDefault:"postgresql://architect:09970207@192.168.150.139:5444/medecine?sslmode=disable"`
 }
 
 func NewConfig() (*Config, error) {
